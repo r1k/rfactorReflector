cc9cc432017cf1f7bfa99807959f3afdc8cdf97f
Kelsey <r.i.kelsey@gmail.com>
2014-01-02 20:31:21 +0000

    update test code and print debug

--- rFactor/client.py
+++ rFactor/client.py
@@ -11,6 +11,7 @@ def post(url, post_data_dictionary):
     post_data_encoded = urllib.urlencode(post_data_dictionary)
     request_object = urllib2.Request(url, post_data_encoded,http_headers)
     response = urllib2.urlopen(request_object)
+    print response
 
 if __name__ == '__main__':
 
@@ -23,3 +24,5 @@ if __name__ == '__main__':
                             "firstSector":1,
                             "secondSector":3,
                             "totalTime":5 }
+
+    post(url, post_data_dictionary)
